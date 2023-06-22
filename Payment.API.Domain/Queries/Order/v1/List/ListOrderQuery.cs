aa63f63d3d4406fff94b1860777a064250db1ef2
matheus7k <06matheus.oliveira@gmail.com>
2023-06-22 13:35:32 -0300

    Adicionado metodo ListOrders

---
+++ Payment.API.Domain/Queries/Order/v1/List/ListOrderQuery.cs
@@ -0,0 +1,19 @@
+﻿using Payment.API.Domain.Contracts.v1;
+
+namespace Payment.API.Domain.Queries.Order.v1.List
+{
+    public class ListOrderQuery
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public ListOrderQuery(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<IEnumerable<Entities.v1.Order>> List()
+        {
+            return await _orderRepository.GetAllAsync();
+        }
+    }
+}
--- Payment.API/Controllers/OrderController.cs
+++ Payment.API/Controllers/OrderController.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Payment.API.Domain.Commands.Order.v1.Create;
-using Payment.API.Domain.Commands.Payment.v1.Create;
+using Payment.API.Domain.Queries.Order.v1.List;
 
 namespace Payment.API.Controllers
 {
@@ -10,16 +9,24 @@ namespace Payment.API.Controllers
     public class OrderController : ControllerBase
     {
         private readonly CreateOrderCommandHandler _orderHandler;
+        private readonly ListOrderQuery _listOrder;
 
-        public OrderController(CreateOrderCommandHandler orderhandler)
+        public OrderController(CreateOrderCommandHandler orderhandler, ListOrderQuery listOrder)
         {
             _orderHandler = orderhandler;
+            _listOrder = listOrder;
         }
 
         [HttpPost(Name = "CreateOrder")]
         public Task<Domain.Entities.v1.Order> InsertOrder([FromBody] CreateOrderCommand command)
         {
             return _orderHandler.Insert(command);
+        }
+
+        [HttpGet(Name = "ListOrders")]
+        public Task<IEnumerable<Domain.Entities.v1.Order>> GetOrders()
+        {
+            return _listOrder.List();
         }
     }
 }
